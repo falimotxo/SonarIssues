97313255932207cfe1fa2b477e1ca5554a6c658a
falimotxo <rmartinpriego@gmail.com>
2017-08-31 12:27:31 +0200

    Nuevo diseño para la tabla de resumen para el responsable. Nuevo perfil para los usuarios para que solo reciba el resumen (añadiendo *)

--- SonarIssues.py
+++ SonarIssues.py
@@ -18,9 +18,16 @@ STR_CREATED_IN_LAST = 'created_in_last'
 STR_ADDRESS_EMAIL = 'address_email'
 STR_PORT_EMAIL = 'port_email'
 
+TOTAL = 'total'
+BLOCKER = 'blocker'
+CRITICAL = 'critical'
+MAJOR = 'major'
+MINOR = 'minor'
+INFO = 'info'
+
 print('INFO - {} : ****** Iniciando generador de informes de incidencias de Sonar ******'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
 print('INFO - {} : Inicio obtención de parámetros de configuración'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
-#INICIO OBTENCIÓN DE PARÁMETROS DE CONFIGURACIÓN
+# INICIO OBTENCIÓN DE PARÁMETROS DE CONFIGURACIÓN
 
 # Se recoge la dirección y contraseña del email por parámetro de consola de comandos
 print('INFO - {} : Obteniendo parámetros de configuración por consola de comandos'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
@@ -71,7 +78,7 @@ print('INFO - {} : Parámetros de configuración de login obtenidos correctament
 print('INFO - {} : Obteniendo parámetros de configuración de búsqueda de incidencias'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
 
 for item in config.items("PARAM_SEARCH"):
-    if (item[0] == STR_CREATED_IN_LAST):
+    if item[0] == STR_CREATED_IN_LAST:
         createdInLast = item[1]
 
 try:
@@ -80,11 +87,11 @@ except NameError:
     print('ERROR - {} : Fecha de consulta no definido en el archivo de configuración'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
     exit(1)
 
-if(createdInLast is None or createdInLast == ''):
+if createdInLast is None or createdInLast == '':
     print('ERROR - {} : Parámetros de búsqueda no definidas en el archivo de configuración'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
     exit(1)
 
-#Traducimos el tiempo de consulta para mostrarlo en el email
+# Traducimos el tiempo de consulta para mostrarlo en el email
 strCreatedInLast = ''
 splitCreatedInLast = re.split('([0-9]+[a-z]+)', createdInLast)
 ifFirstIteration = True
@@ -122,26 +129,35 @@ print('INFO - {} : Obteniendo usuarios'.format(datetime.datetime.now().strftime(
 
 listUsers = []
 listResp = []
+listRespOnlyRes = []
 mapGroupUsers = dict()
 mapRespUsers = dict()
 for item in config.items("USERS_GROUP"):
     resp = item[0]
     users_resp = item[1]
 
-    #A los responsables no se le genera informe
+    # A los responsables no se le genera informe
     for i in resp.split(','):
-        if i not in listResp:
-            listResp.append(i)
+        r = i
+        # Si el responsable tiene un * al final, significa que solo se le debe de enviar el resumen de su equipo.
+        if '*' in i:
+            temp = len(i)
+            r = i[:temp - 1]
+            if r not in listRespOnlyRes:
+                listRespOnlyRes.append(r)
+
+        if r not in listResp:
+            listResp.append(r)
 
     for i in users_resp.split(','):
         if i not in listUsers:
             listUsers.append(i)
             if i not in mapRespUsers:
-                mapRespUsers[i] = resp
+                mapRespUsers[i] = resp.replace('*', '')
 
-    mapGroupUsers[resp] = users_resp
+    mapGroupUsers[resp.replace('*', '')] = users_resp
 
-if(len(listUsers) <= 0 or len(listResp) <= 0 or len(mapGroupUsers) <= 0):
+if len(listUsers) <= 0 or len(listResp) <= 0 or len(mapGroupUsers) <= 0:
     print('ERROR - {} : Usuarios no definidos en el archivo de configuración'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
     exit(1)
 
@@ -149,9 +165,9 @@ print('INFO - {} : Usuarios obtenidos correctamente'.format(datetime.datetime.no
 print('INFO - {} : Obteniendo parámetros de configuración de envío de email'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
 
 for item in config.items("EMAIL"):
-    if (item[0] == STR_ADDRESS_EMAIL):
+    if item[0] == STR_ADDRESS_EMAIL:
         address_email = item[1]
-    elif (item[0] == STR_PORT_EMAIL):
+    elif item[0] == STR_PORT_EMAIL:
         port_email = item[1]
 
 try:
@@ -166,18 +182,18 @@ except NameError:
     print('ERROR - {} : Puerto servidor de correo no definido en el archivo de configuración'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
     exit(1)
 
-if(address_email is None or address_email == '' or port_email is None or port_email == ''):
+if address_email is None or address_email == '' or port_email is None or port_email == '':
     print('ERROR - {} : Parámetros de servidor de correo no definidas en el archivo de configuración'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
     exit(1)
 
 print('INFO - {} : Parámetros de configuración de envío de email obtenidos correctamente'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
 print('INFO - {} : Parámetros de configuración obtenidos correctamente'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
 
-#FIN OBTENCIÓN DE PARÁMETROS DE CONFIGURACIÓN
+# FIN OBTENCIÓN DE PARÁMETROS DE CONFIGURACIÓN
 print('INFO - {} : Inicio generación de informes de incidencias de Sonar'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
 
-# Map <Usuario - Mensaje común> para almacenar la información por usuario que se enviará al responsable al finalizar
-mapMessageComunByUser = dict()
+# Map <Usuario - Map Issues> para almacenar la información de las incidencias por usuario que se enviará al responsable al finalizar
+mapIssuesByUser = dict()
 # Map <Usuario - Mensaje> para almacenar la información por usuario que se enviará al correo
 mapMessageByUser = dict()
 # Mapa de información de personas
@@ -191,11 +207,13 @@ for resp in listResp:
     if response.status_code == 200:
         data = response.json()
 
-        if data['total'] == 1:
+        if data['total'] > 0:
             person = ''
             for i in data['users']:
-                person = User.as_payload(i)
-                mapInfoUser[resp] = person
+                if i['login'] == resp:
+                    person = User.as_payload(i)
+                    mapInfoUser[resp] = person
+                    break
 
 for user in listUsers:
     payload = {'q': user}
@@ -203,21 +221,23 @@ for user in listUsers:
     if response.status_code == 200:
         data = response.json()
 
-        if data['total'] == 1:
+        if data['total'] > 0:
             person = ''
             for i in data['users']:
-                person = User.as_payload(i)
-                mapInfoUser[user] = person
+                if i['login'] == user:
+                    person = User.as_payload(i)
+                    mapInfoUser[user] = person
+                    break
 
-            payload = {'authors' : user, 'createdInLast' : createdInLast, 'ps' : 500}
+            payload = {'authors': user, 'createdInLast': createdInLast, 'ps': 500}
             response = requests.get('https://qamind.indra.es/api/issues/search', params=payload, auth=(user_sonar, password_sonar), verify=False)
 
             if response.status_code == 200:
                 print('INFO - {} : ****** Iniciando generación de informe para el usuario {} ******'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y'), user))
                 messageHtml = '<h1>Informe Incidencias Sonar</h1>'
 
-                messageComunHtml = '<table border="1" style="border-collapse: collapse; border: 1px solid black;"><tr>'
-                messageComunHtml += '<th colspan="3" style="background-color: #00bfff;">Usuario: ' + person.name + ' (' + user + ')</th></tr>'
+                messageHtml += '<table border="1" style="border-collapse: collapse; border: 1px solid black;"><tr>'
+                messageHtml += '<th colspan="3" style="background-color: #00bfff;">Usuario: ' + person.name + ' (' + user + ')</th></tr>'
 
                 data = response.json()
 
@@ -227,7 +247,7 @@ for user in listUsers:
                 if total % ps > 0:
                     num_pages += 1
 
-                messageComunHtml += '<tr style="font-weight: bold;"><td>Total incidencias en periodo de observacion (' + strCreatedInLast + ')</td><td style="width: 25px; text-align: center;"> ' + str(total) + '</td></tr>'
+                messageHtml += '<tr style="font-weight: bold;"><td>Total incidencias en periodo de observacion (' + strCreatedInLast + ')</td><td style="width: 25px; text-align: center;"> ' + str(total) + '</td></tr>'
 
                 issueList = []
                 for i in data['issues']:
@@ -235,24 +255,37 @@ for user in listUsers:
 
                 if num_pages > 1:
                     for i in range(1, num_pages):
-                        payload = {'authors' : user, 'createdInLast' : createdInLast, 'ps' : 500,  'p' : (i+1)}
+                        payload = {'authors': user, 'createdInLast': createdInLast, 'ps': 500,  'p': (i+1)}
                         response = requests.get('https://qamind.indra.es/api/issues/search', params=payload, auth=(user_sonar, password_sonar), verify=False)
                         data = response.json()
-                        for i in data['issues']:
-                            issueList.append(Issues.as_payload(i))
+                        for iss in data['issues']:
+                            issueList.append(Issues.as_payload(iss))
 
                 issueSonar = IssueSonar(issueList)
 
                 numIssuesBlocker = issueSonar.getNumIssuesBlocker()
                 numIssuesCritical = issueSonar.getNumIssuesCritical()
-                messageComunHtml += '<tr style="font-weight: bold; color: red;"><td>Número de incidencias BLOCKER</td><td style="width: 25px; text-align: center;">' + str(numIssuesBlocker) + '</td></tr>'
-                messageComunHtml += '<tr style="font-weight: bold; color: red;"><td>Número de incidencias CRITICAL</td><td style="width: 25px; text-align: center;">' + str(numIssuesCritical) + '</td></tr>'
-                messageComunHtml += '<tr><td style="">Número de incidencias MAJOR</td><td style="width: 25px; text-align: center;">' + str(issueSonar.getNumIssuesMajor()) + '</td></tr>'
-                messageComunHtml += '<tr><td style="">Número de incidencias MINOR</td><td style="width: 25px; text-align: center;">' + str(issueSonar.getNumIssuesMinor()) + '</td></tr>'
-                messageComunHtml += '<tr><td style="">Número de incidencias INFO</td><td style="width: 25px; text-align: center;">' +  str(issueSonar.getNumIssuesInfo()) + '</td></tr>'
-                messageComunHtml += '</table>'
+                numIssuesMajor = issueSonar.getNumIssuesMajor()
+                numIssuesMinor = issueSonar.getNumIssuesMinor()
+                numIssuesInfo = issueSonar.getNumIssuesInfo()
 
-                messageHtml += messageComunHtml
+                messageHtml += '<tr style="font-weight: bold; color: red;"><td>Número de incidencias BLOCKER</td><td style="width: 25px; text-align: center;">' + str(numIssuesBlocker) + '</td></tr>'
+                messageHtml += '<tr style="font-weight: bold; color: red;"><td>Número de incidencias CRITICAL</td><td style="width: 25px; text-align: center;">' + str(numIssuesCritical) + '</td></tr>'
+                messageHtml += '<tr><td style="">Número de incidencias MAJOR</td><td style="width: 25px; text-align: center;">' + str(numIssuesMajor) + '</td></tr>'
+                messageHtml += '<tr><td style="">Número de incidencias MINOR</td><td style="width: 25px; text-align: center;">' + str(numIssuesMinor) + '</td></tr>'
+                messageHtml += '<tr><td style="">Número de incidencias INFO</td><td style="width: 25px; text-align: center;">' +  str(numIssuesInfo) + '</td></tr>'
+                messageHtml += '</table></br>'
+
+                # Map <Tipo Issue - Número Issue> para almacenar la cantidad de incidencias por tipo de un usuario
+                mapInfoIssuesByUser = dict()
+                mapInfoIssuesByUser[TOTAL] = total
+                mapInfoIssuesByUser[CRITICAL] = numIssuesCritical
+                mapInfoIssuesByUser[BLOCKER] = numIssuesBlocker
+                mapInfoIssuesByUser[MAJOR] = numIssuesMajor
+                mapInfoIssuesByUser[MINOR] = numIssuesMinor
+                mapInfoIssuesByUser[INFO] = numIssuesInfo
+                mapIssuesByUser[user] = mapInfoIssuesByUser
+
                 messageHtml += '<dl><dt><b>Clases afectadas:</b></dt>'
                 messageHtml += issueSonar.getAffectedClass()
                 messageHtml += '</dl></br>'
@@ -261,7 +294,7 @@ for user in listUsers:
                 messageHtml += issueSonar.getIssuesClasification()
                 messageHtml += '</dl></br>'
 
-                #Imprimimos las incidencias BLOCKER Y CRITICAL, en el caso que existan
+                # Imprimimos las incidencias BLOCKER Y CRITICAL, en el caso que existan
                 if numIssuesBlocker > 0 or numIssuesCritical > 0:
                     messageHtml += '<table border="1" style="border-collapse: collapse; border: 1px solid black;">'
                     messageHtml += '<tr><th style="background-color: #00bfff;">Incidencias BLOCKER/CRITICAL</th></tr>'
@@ -273,8 +306,8 @@ for user in listUsers:
                 mapDebtByUser[user] = accumulatedDebt
                 messageHtml += '<p><b>Deuda técnica acumulada en el periodo de observación (' + strCreatedInLast + '): ' + issueSonar.parseDebtToFormat(accumulatedDebt) + '</p></b>'
 
-                #Obtener todas las incidencias existentes para poder calcular la deuda total
-                payload = {'authors': user, 'ps' : 500}
+                # Obtener todas las incidencias existentes para poder calcular la deuda total
+                payload = {'authors': user, 'ps': 500}
                 response = requests.get('https://qamind.indra.es/api/issues/search', params=payload, auth=(user_sonar, password_sonar), verify=False)
                 data = response.json()
                 total = data["total"]
@@ -288,21 +321,17 @@ for user in listUsers:
 
                 if num_pages > 1:
                     for i in range(1, num_pages):
-                        payload = {'authors': user, 'ps' : 500, 'p': (i + 1)}
+                        payload = {'authors': user, 'ps': 500, 'p': (i + 1)}
                         response = requests.get('https://qamind.indra.es/api/issues/search', params=payload, auth=(user_sonar, password_sonar), verify=False)
                         data = response.json()
-                        for i in data['issues']:
-                            issueList.append(Issues.as_payload(i))
+                        for iss in data['issues']:
+                            issueList.append(Issues.as_payload(iss))
 
                 issueSonar = IssueSonar(issueList)
 
                 accumulatedDebt = issueSonar.getAccumulatedDebt()
                 messageHtml += '<p><b>Deuda técnica acumulada en todos los proyectos: ' + issueSonar.parseDebtToFormat(accumulatedDebt) + '</p></b>'
 
-                # Añadir enlace a Sonar
-
-                # Se inserta la información común a enviar al responsable
-                mapMessageComunByUser[user] = messageComunHtml
                 # Se inserta la información a enviar al usuario
                 mapMessageByUser[user] = messageHtml
 
@@ -337,24 +366,24 @@ if len(mapDebtByUser) > 0:
         segundos = seconds - ((dias * 86400) + (horas * 3600) + (minutos * 60))
         formatDebt = str(dias) + "D " + str(horas) + "h " + str(minutos) + "m " + str(segundos) + "s"
         rankingHtml += '<tr><td style="text-align: center;">' + str(count) + '</td><td>' + orderUser + '</td><td>' + formatDebt + '</td></tr>'
-        count+=1
+        count += 1
         # Mostramos los 5 primeros del ranking
         if count > 5:
             break
 
     rankingHtml += '</table>'
 
-# Envíamos emails a los usuarios
+# Enviamos emails a los usuarios
 print('INFO - {} : ****** Inicio envíos de email a los usuarios ******'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
 for user in listUsers:
     if user in mapInfoUser:
         infoUser = mapInfoUser[user]
-        #Hay que obtener los email de los responsables para enviar el correo con CC
+        # Hay que obtener los email de los responsables para enviar el correo con CC
         emailResp = ''
         if user in mapRespUsers:
             respUser = mapRespUsers[user]
             for i in respUser.split(','):
-                if i in mapInfoUser:
+                if i in mapInfoUser and i not in listRespOnlyRes:
                     infoResp = mapInfoUser[i]
                     emailResp += infoResp.email + ','
 
@@ -374,13 +403,30 @@ print('INFO - {} : ****** Envío de email a los usuarios finalizado correctament
 # Recorremos el mapa de grupos para obtener a los responsables y enviar la info común de los usuarios que les corresponde
 print('INFO - {} : ****** Inicio generación de informe a responsables ******'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
 for k in mapGroupUsers.keys():
+    emailResp = ''
     listUsersResp = mapGroupUsers[k]
     messageRespHtml = '<h1>Informe resúmen Incidencias Sonar</h1>'
-    emailResp = ''
+    messageRespHtml += '<table border="1" style="border-collapse: collapse; border: 1px solid black;">'
+    messageRespHtml += '<tr><td style="visibility: hidden;"></td>'
+    messageRespHtml += '<td style="padding-left: 5px; padding-right: 5px; text-align: center; width: 75px; background-color: #00bfff;">TOTAL</td>'
+    messageRespHtml += '<td style="padding-left: 5px; padding-right: 5px; text-align: center; width: 75px; background-color: #00bfff;">BLOCKER</td>'
+    messageRespHtml += '<td style="padding-left: 5px; padding-right: 5px; text-align: center; width: 75px; background-color: #00bfff;">CRITICAL</td>'
+    messageRespHtml += '<td style="padding-left: 5px; padding-right: 5px; text-align: center; width: 75px; background-color: #00bfff;">MAJOR</td>'
+    messageRespHtml += '<td style="padding-left: 5px; padding-right: 5px; text-align: center; width: 75px; background-color: #00bfff;">MINOR</td>'
+    messageRespHtml += '<td style="padding-left: 5px; padding-right: 5px; text-align: center; width: 75px; background-color: #00bfff;">INFO</td></tr>'
+
     for u in listUsersResp.split(','):
-        if u in mapMessageComunByUser:
-            messageRespHtml += mapMessageComunByUser[u]
-            messageRespHtml += '</br>'
+        infoUser = mapInfoUser[u]
+        if u in mapIssuesByUser:
+            mapNumIssues = mapIssuesByUser[u]
+            messageRespHtml += '<tr><td style="padding-right: 5px; background-color: #00bfff;">' + infoUser.name + ' (' + u + ')</td>'
+            messageRespHtml += '<td style="text-align: center;">' + str(mapNumIssues[TOTAL]) + '</td>'
+            messageRespHtml += '<td style="text-align: center;">' + str(mapNumIssues[BLOCKER]) + '</td>'
+            messageRespHtml += '<td style="text-align: center;">' + str(mapNumIssues[CRITICAL]) + '</td>'
+            messageRespHtml += '<td style="text-align: center;">' + str(mapNumIssues[MAJOR]) + '</td>'
+            messageRespHtml += '<td style="text-align: center;">' + str(mapNumIssues[MINOR]) + '</td>'
+            messageRespHtml += '<td style="text-align: center;">' + str(mapNumIssues[INFO]) + '</td></tr>'
+    messageRespHtml += '</table>'
 
     for r in k.split(','):
         if r in mapInfoUser:
@@ -390,4 +436,4 @@ for k in mapGroupUsers.keys():
 
 print('INFO - {} : ****** Finalizado generación de informe a responsables ******'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
 print('INFO - {} : ****** Generador de informes de incidencias de Sonar finalizado correctamente ******'.format(datetime.datetime.now().strftime('%H:%M:%S %d/%m/%Y')))
-#exit(0)
+# exit(0)
